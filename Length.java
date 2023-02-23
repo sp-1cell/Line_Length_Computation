fd25db1d98bae031b60932af2b42ecc237b4a917
sp-1cell <sushant6247@gmail.com>
2023-02-23 11:54:59 +0530

    CODE FOR SHOWING WELCOME MESSAGE

---
+++ Length.java
@@ -0,0 +1,9 @@
+package LenghthCom;
+
+public class Length {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO LINE COMPARISION COMPUTATION PROGRAM");
+
+
+    }
+}
