5fb62faad7ae83c53926905a499af13d5d521702
sp-1cell <sushant6247@gmail.com>
2023-02-23 12:00:57 +0530

    CALCULATING LENGTH

--- Length.java
+++ Length.java
@@ -1,9 +1,23 @@
 package LenghthCom;
 
+import java.util.Scanner;
+
 public class Length {
     public static void main(String[] args) {
-        System.out.println("WELCOME TO LINE COMPARISION COMPUTATION PROGRAM");
 
-
+        Scanner t = new Scanner(System.in);
+        System.out.println("Enter Coordinate of X1, Y1");
+        int x1 = t.nextInt();
+        int y1 = t.nextInt();
+        System.out.println("Enter Coordinate of X2, Y2");
+        int x2 = t.nextInt();
+        int y2 = t.nextInt();
+        double lengthof = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+        System.out.println("lengthof line" + lengthof);
     }
 }
+
+
+
+
+
